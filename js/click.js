9ae86d18a0cf1b8e25a46de8ede280ce9d0453d9
James Shiffer <basedvioletfag@waifu.club>
2020-06-27 00:22:35 -0700

    attempt to make click sound IE-compatible

--- js/click.js
+++ js/click.js
@@ -1,13 +1,31 @@
-var links = document.querySelectorAll("a");
-links.forEach(function (link) {
-    link.addEventListener("click", function () {
-        var audio = document.createElement("audio");
-        audio.style.display = "none";
-        audio.src = "assets/click.wav";
-        audio.autoplay = true;
-        audio.onended = function() {
-            audio.remove();
-        };
-        document.body.appendChild(audio);
-    });
-});
+function playAudio(e) {
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+    var audio = document.createElement("audio");
+    audio.style.display = "none";
+    audio.src = "assets/click.wav";
+    audio.autoplay = true;
+    audio.onended = function () {
+        audio.remove();
+        if (e.target) {
+            window.location.href = e.target.href;
+        }
+    };
+    document.body.appendChild(audio);
+}
+
+function on(el, event, cb) {
+    if (el.addEventListener) {
+        el.addEventListener(event, cb, true); 
+    } else if (el.attachEvent)  {
+        el.attachEvent("on" + event, cb);
+    } else {
+        el["on" + event] = cb;
+    }
+}
+
+for (var i = 0; i < document.links.length; i++) {
+    var link = document.links[i];
+    on(link, "click", playAudio);
+}
